43fb5f4dd5df375539094f09c89bbe0a26598bd4
sbaenat <sbaenat@gmail.com>
2017-10-05 06:46:55 -0500

    Correcciones

--- Assets/Codigo/JumpSound.cs
+++ Assets/Codigo/JumpSound.cs
@@ -8,13 +8,11 @@ public class JumpSound : MonoBehaviour {
 
     public AudioSource JumpSource;
 
-	// Use this for initialization
 	void Start () {
         JumpSource.clip = JumpClip;
 
     }
 	
-	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Space))
             JumpSource.Play();
